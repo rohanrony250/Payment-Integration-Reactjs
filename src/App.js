af8c61bd72b765aee60ec0c48615e38d035f6a5c
RohanRony <rohanrony@hotmail.com>
2021-01-19 03:45:59 +0300

    fixed "background-attachment:fixed" scroll bug

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App()
   return (
     
     <div>
-      {/* <NavbarComponent /> */}
+      
       <HeaderComponent />
         <Switch>
           <Route exact path='/' component={AboutSec}/>
